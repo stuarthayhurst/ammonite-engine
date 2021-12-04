551bdd52d3ee051c3e9316aa60e898b79d491e31
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-12-04 15:09:28 +0000

    Finished tutorial 3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
 
 #include "common/loadShader.h"
 
-using namespace glm;
+int width = 1024;
+int height = 768;
+char title[] = "Tutorial 3";
 
 int main() {
   //Initialise GLFW
@@ -23,7 +29,7 @@ int main() {
 
   //Open a window and create its OpenGL context
   GLFWwindow* window;
-  window = glfwCreateWindow(1024, 768, "Tutorial 02", NULL, NULL);
+  window = glfwCreateWindow(width, height, title, NULL, NULL);
   if (window == NULL) {
     fprintf(stderr, "Failed to open window");
     glfwTerminate();
@@ -45,6 +51,28 @@ int main() {
   glGenVertexArrays(1, &VertexArrayID);
   glBindVertexArray(VertexArrayID);
 
+  //Create and compile shaders
+  GLuint programID = LoadShaders("shaders/SimpleVertexShader.vert", "shaders/SimpleFragmentShader.frag");
+
+
+  //Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+  glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) width / (float)height, 0.1f, 100.0f);
+
+  //Camera matrix
+  glm::mat4 View = glm::lookAt(
+    glm::vec3(4,3,3), //Camera is at (4,3,3), in World Space
+    glm::vec3(0,0,0), //and looks at the origin
+    glm::vec3(0,1,0)  //Head is up (set to 0,-1,0 to look upside-down)
+  );
+
+  //Model matrix : an identity matrix (model be at the origin)
+  glm::mat4 Model = glm::mat4(1.0f);
+  //Our ModelViewProjection : multiplication of our 3 matrices
+  glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+  //Get an ID for the model view projection
+  GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
   //An array of 3 vectors which represents 3 vertices
   static const GLfloat g_vertex_buffer_data[] = {
     -1.0f, -1.0f, 0.0f,
@@ -52,35 +80,35 @@ int main() {
     0.0f,  1.0f, 0.0f,
   };
 
-  //This will identify our vertex buffer
+  //Create a vertex buffer
   GLuint vertexbuffer;
-  //Generate 1 buffer, put the resulting identifier in vertexbuffer
   glGenBuffers(1, &vertexbuffer);
-  //The following commands will talk about our 'vertexbuffer' buffer
   glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-  //Give our vertices to OpenGL.
+  //Give vertices to OpenGL
   glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-  //Create and compile shaders
-  GLuint programID = LoadShaders("shaders/SimpleVertexShader.vert", "shaders/SimpleFragmentShader.frag");
 
   //Loop until window closed
   while(glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0) {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    //Use the shaders
+    glUseProgram(programID);
+    //Send the transformation to the current shader in "MVP" uniform
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+
     //1st attribute buffer : vertices
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glVertexAttribPointer(
-      0,                  //attribute 0. No particular reason for 0, but must match the layout in the shader.
-      3,                  //size
-      GL_FLOAT,           //type
-      GL_FALSE,           //normalized?
-      0,                  //stride
-      (void*)0            //array buffer offset
+      0,        //attribute 0. No particular reason for 0, but must match the layout in the shader
+      3,        //size
+      GL_FLOAT, //type
+      GL_FALSE, //normalized?
+      0,        //stride
+      (void*)0  //array buffer offset
     );
-    //Draw the triangle !
-    glUseProgram(programID);
+
+    //Draw triangle
     glDrawArrays(GL_TRIANGLES, 0, 3); //Starting from vertex 0; 3 vertices total -> 1 triangle
     glDisableVertexAttribArray(0);
 
@@ -88,4 +116,12 @@ int main() {
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+    //Cleanup VBO, shader and window
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteProgram(programID);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    glfwTerminate();
+
+    return 0;
 }
