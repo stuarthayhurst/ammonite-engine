ed68789fffc4f867b072cd7f47d671e91fe077d5
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-02-18 16:19:10 +0000

    Added pause and unpause methods to timer

--- src/common/timer.hpp
+++ src/common/timer.hpp
@@ -7,13 +7,36 @@ namespace timer {
   class timer {
     public:
       double getTime() {
-        return glfwGetTime() - start;
+        if (running) {
+          return glfwGetTime() - start - offset;
+        } else {
+          return glfwGetTime() - start - offset - (glfwGetTime() - stopTime);
+        }
       }
       void reset() {
+        running = true;
         start = glfwGetTime();
+        stopTime = 0;
+        offset = 0;
+      }
+
+      void pause() {
+        if (running) {
+          stopTime = glfwGetTime();
+          running = false;
+        }
+      }
+      void unpause() {
+        if (!running) {
+          offset += glfwGetTime() - stopTime;
+          running = true;
+        }
       }
     private:
+      bool running = true;
       double start = glfwGetTime();
+      double stopTime = 0;
+      double offset = 0;
   };
 }
 
