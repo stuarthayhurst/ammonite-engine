775a7dd08553f23fab116a85fb7ebb23fc752bb7
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-02-20 22:45:03 +0000

    Actually unbind textures

--- src/ammonite/textures.cpp
+++ src/ammonite/textures.cpp
@@ -45,7 +45,7 @@ namespace ammonite {
       //Generate mipmaps
       glGenerateMipmap(GL_TEXTURE_2D);
 
-      //glBindTexture(GL_TEXTURE_2D, 0);
+      glBindTexture(GL_TEXTURE_2D, 0);
       return textureId;
     }
   }
