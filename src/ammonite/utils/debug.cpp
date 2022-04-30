e77aae4269f489c821db66428eecad4b9c7e59b1
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-04-30 20:58:45 +0100

    Make OpenGL callback static

--- src/ammonite/utils/debug.cpp
+++ src/ammonite/utils/debug.cpp
@@ -6,7 +6,7 @@
 namespace ammonite {
   namespace utils {
     namespace {
-      void GLAPIENTRY debugMessageCallback(GLenum, GLenum type, GLuint, GLenum severity, GLsizei, const GLchar* message, const void*) {
+      static void GLAPIENTRY debugMessageCallback(GLenum, GLenum type, GLuint, GLenum severity, GLsizei, const GLchar* message, const void*) {
         std::cerr << "\nGL CALLBACK: " << (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "") << std::endl;
         std::cerr << "  Type: 0x" << type << std::endl;
         std::cerr << "  Severity: 0x" << severity << std::endl;
