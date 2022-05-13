43413338904df2bea2414892f5808d1ef088a261
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-05-13 21:17:43 +0100

    Compress textures when uploaded

--- src/ammonite/textures.cpp
+++ src/ammonite/textures.cpp
@@ -66,9 +66,9 @@ namespace ammonite {
       //Select and pass texture to OpenGL
       glBindTexture(GL_TEXTURE_2D, textureId);
       if (bpp == 3) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_COMPRESSED_RGB_ARB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
       } else if (bpp == 4) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_COMPRESSED_RGBA_ARB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
       } else {
         std::cerr << "Failed to load texture '" << texturePath << "'" << std::endl;
         glDeleteTextures(1, &textureId);
