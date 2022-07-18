88ef5fd7ee28d5f1f5cd74249c6857ca5abdffb4
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2022-07-18 16:31:07 +0100

    Store skybox coords as chars, instead of floats

--- src/ammonite/renderer.cpp
+++ src/ammonite/renderer.cpp
@@ -203,48 +203,48 @@ namespace ammonite {
         //Get the max number of lights supported
         maxLightCount = ammonite::lighting::getMaxLightCount();
 
-        const float skyboxVertices[] = {
-          -1.0f,  1.0f, -1.0f,
-          -1.0f, -1.0f, -1.0f,
-           1.0f, -1.0f, -1.0f,
-           1.0f, -1.0f, -1.0f,
-           1.0f,  1.0f, -1.0f,
-          -1.0f,  1.0f, -1.0f,
+        const char skyboxVertices[] = {
+          -1,  1, -1,
+          -1, -1, -1,
+           1, -1, -1,
+           1, -1, -1,
+           1,  1, -1,
+          -1,  1, -1,
 
-          -1.0f, -1.0f,  1.0f,
-          -1.0f, -1.0f, -1.0f,
-          -1.0f,  1.0f, -1.0f,
-          -1.0f,  1.0f, -1.0f,
-          -1.0f,  1.0f,  1.0f,
-          -1.0f, -1.0f,  1.0f,
+          -1, -1,  1,
+          -1, -1, -1,
+          -1,  1, -1,
+          -1,  1, -1,
+          -1,  1,  1,
+          -1, -1,  1,
 
-           1.0f, -1.0f, -1.0f,
-           1.0f, -1.0f,  1.0f,
-           1.0f,  1.0f,  1.0f,
-           1.0f,  1.0f,  1.0f,
-           1.0f,  1.0f, -1.0f,
-           1.0f, -1.0f, -1.0f,
+           1, -1, -1,
+           1, -1,  1,
+           1,  1,  1,
+           1,  1,  1,
+           1,  1, -1,
+           1, -1, -1,
 
-          -1.0f, -1.0f,  1.0f,
-          -1.0f,  1.0f,  1.0f,
-           1.0f,  1.0f,  1.0f,
-           1.0f,  1.0f,  1.0f,
-           1.0f, -1.0f,  1.0f,
-          -1.0f, -1.0f,  1.0f,
+          -1, -1,  1,
+          -1,  1,  1,
+           1,  1,  1,
+           1,  1,  1,
+           1, -1,  1,
+          -1, -1,  1,
 
-          -1.0f,  1.0f, -1.0f,
-           1.0f,  1.0f, -1.0f,
-           1.0f,  1.0f,  1.0f,
-           1.0f,  1.0f,  1.0f,
-          -1.0f,  1.0f,  1.0f,
-          -1.0f,  1.0f, -1.0f,
+          -1,  1, -1,
+           1,  1, -1,
+           1,  1,  1,
+           1,  1,  1,
+          -1,  1,  1,
+          -1,  1, -1,
 
-          -1.0f, -1.0f, -1.0f,
-          -1.0f, -1.0f,  1.0f,
-           1.0f, -1.0f, -1.0f,
-           1.0f, -1.0f, -1.0f,
-          -1.0f, -1.0f,  1.0f,
-           1.0f, -1.0f,  1.0f
+          -1, -1, -1,
+          -1, -1,  1,
+           1, -1, -1,
+           1, -1, -1,
+          -1, -1,  1,
+           1, -1,  1
         };
 
         //Create vertex buffer and array for skybox
@@ -254,7 +254,7 @@ namespace ammonite {
         glBindBuffer(GL_ARRAY_BUFFER, skyboxBuffer);
         glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        glVertexAttribPointer(0, 3, GL_BYTE, GL_FALSE, 3 * sizeof(char), nullptr);
       }
     }
 
