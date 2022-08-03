cbf829bf49b08486225e0be2c74dede03dadc7c0
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2022-08-03 18:29:27 +0100

    Make init time specific to each timer, fixes timers not resetting to 0

--- src/ammonite/utils/timer.hpp
+++ src/ammonite/utils/timer.hpp
@@ -11,7 +11,10 @@ namespace ammonite {
         auto systemTimeNano = std::chrono::duration_cast<std::chrono::nanoseconds>(systemTime).count();
         return systemTimeNano;
       }
+    }
 
+    class Timer {
+    private:
       static double getTimeDelta() {
         static const double initTime = getNanoTime();
         double systemTime = getNanoTime() - initTime;
@@ -19,48 +22,45 @@ namespace ammonite {
         //Convert nanoseconds into seconds
         return systemTime / 1000000000;
       }
-    }
 
-    class Timer {
-      public:
-        double getTime() {
-          if (isTimerRunning) {
-            return getTimeDelta() - startTime - offset;
-          } else {
-            //If the timer hasn't been unpaused yet, correct for the time
-            return stopTime - startTime - offset;
-          }
+      bool isTimerRunning = true;
+      double startTime = getTimeDelta();
+      double stopTime = 0.0;
+      double offset = 0.0;
+
+    public:
+      double getTime() {
+        if (isTimerRunning) {
+          return getTimeDelta() - startTime - offset;
+        } else {
+          //If the timer hasn't been unpaused yet, correct for the time
+          return stopTime - startTime - offset;
         }
+      }
 
-        bool isRunning() {
-          return isTimerRunning;
+      bool isRunning() {
+        return isTimerRunning;
+      }
+
+      void reset() {
+        startTime = getTimeDelta();
+        stopTime = 0.0;
+        offset = 0.0;
+      }
+
+      void pause() {
+        if (isTimerRunning) {
+          stopTime = getTimeDelta();
+          isTimerRunning = false;
         }
+      }
 
-        void reset() {
-          startTime = getTimeDelta();
-          stopTime = 0.0;
-          offset = 0.0;
+      void unpause() {
+        if (!isTimerRunning) {
+          offset += getTimeDelta() - stopTime;
+          isTimerRunning = true;
         }
-
-        void pause() {
-          if (isTimerRunning) {
-            stopTime = getTimeDelta();
-            isTimerRunning = false;
-          }
-        }
-
-        void unpause() {
-          if (!isTimerRunning) {
-            offset += getTimeDelta() - stopTime;
-            isTimerRunning = true;
-          }
-        }
-
-      private:
-        bool isTimerRunning = true;
-        double startTime = getTimeDelta();
-        double stopTime = 0.0;
-        double offset = 0.0;
+      }
     };
   }
 }
