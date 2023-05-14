5d24da075ac118ad87969c0f8f09780eedcd77c7
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2023-05-14 23:35:27 +0100

    Moved demo setup code into handler

--- src/demos/default.cpp
+++ src/demos/default.cpp
@@ -1,9 +1,66 @@
+#include "../ammonite/ammonite.hpp"
+
 namespace defaultDemo {
+  std::vector<int> loadedModelIds;
+
   int preRendererInit() {
     return 0;
   }
 
   int postRendererInit() {
+    int screenId = ammonite::interface::getActiveLoadingScreen();
+
+    //Load models from a set of objects and textures
+    const char* models[][2] = {
+      {"assets/suzanne.obj", "assets/gradient.png"},
+      {"assets/cube.obj", "assets/flat.png"}
+    };
+    int modelCount = sizeof(models) / sizeof(models[0]);
+
+    bool success = true;
+    long int vertexCount = 0;
+    for (int i = 0; i < modelCount; i++) {
+      //Load model
+      loadedModelIds.push_back(ammonite::models::createModel(models[i][0], &success));
+      vertexCount += ammonite::models::getVertexCount(loadedModelIds[i]);
+      ammonite::models::applyTexture(loadedModelIds[i], models[i][1], true, &success);
+
+      //Update loading screen
+      ammonite::interface::setLoadingScreenProgress(screenId, float(i + 1) / float(modelCount + 1));
+      ammonite::renderer::drawFrame();
+    }
+
+    if (!success) {
+      for (unsigned int i = 0; i < loadedModelIds.size(); i++) {
+        ammonite::models::deleteModel(loadedModelIds[i]);
+      }
+
+      return -1;
+    }
+
+    //Copy last loaded model
+    loadedModelIds.push_back(ammonite::models::copyModel(loadedModelIds[modelCount - 1]));
+    vertexCount += ammonite::models::getVertexCount(loadedModelIds[modelCount]);
+    ammonite::models::position::setPosition(loadedModelIds[modelCount], glm::vec3(4.0f, 4.0f, 4.0f));
+    ammonite::models::position::scaleModel(loadedModelIds[modelCount], 0.25f);
+    modelCount++;
+
+    //Update loading screen
+    ammonite::interface::setLoadingScreenProgress(screenId, 1.0f);
+    ammonite::renderer::drawFrame();
+
+    //Example translation, scale and rotation
+    ammonite::models::position::translateModel(loadedModelIds[0], glm::vec3(-2.0f, 0.0f, 0.0f));
+    ammonite::models::position::scaleModel(loadedModelIds[0], 0.8f);
+    ammonite::models::position::rotateModel(loadedModelIds[0], glm::vec3(0.0f, 0.0f, 0.0f));
+
+    //Set light source properties
+    int lightId = ammonite::lighting::createLightSource();
+    ammonite::lighting::properties::setPower(lightId, 50.0f);
+    ammonite::lighting::linkModel(lightId, loadedModelIds[modelCount - 1]);
+    ammonite::lighting::updateLightSources();
+    ammonite::lighting::setAmbientLight(glm::vec3(0.1f, 0.1f, 0.1f));
+
     return 0;
   }
 
@@ -15,5 +72,3 @@ namespace defaultDemo {
     return 0;
   }
 }
-
-//TODO: fill in
