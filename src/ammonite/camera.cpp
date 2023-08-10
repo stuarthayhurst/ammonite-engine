3b268ff0d724e10b45af9b3c1f8e378909ce4e51
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-08-10 16:54:58 +0100

    Added missing cmath includes

--- src/ammonite/camera.cpp
+++ src/ammonite/camera.cpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <cmath>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
--- src/demos/object-field.cpp
+++ src/demos/object-field.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
 #include <ctime>
+#include <cmath>
 
 #include "../ammonite/ammonite.hpp"
 
