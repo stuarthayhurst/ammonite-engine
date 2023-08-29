8bbb3f015ff9d2f0d17bf36895519318186c1b12
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-08-29 01:35:06 +0100

    Reordered includes in ammonite.hpp

--- src/ammonite/ammonite.hpp
+++ src/ammonite/ammonite.hpp
@@ -1,33 +1,31 @@
 #ifndef AMMONITE
 #define AMMONITE
 
-#include "utils/timer.hpp"
-#include "utils/logging.hpp"
-#include "utils/extension.hpp"
-#include "utils/controls.hpp"
-#include "utils/keybinds.hpp"
+#include "graphics/renderInterface.hpp"
+#include "graphics/shaders.hpp"
+
+#include "lighting/lightInterface.hpp"
+#include "lighting/lightStorage.hpp"
+
+#include "models/modelInterface.hpp"
+#include "models/modelStorage.hpp"
+
 #include "utils/cacheManager.hpp"
+#include "utils/controls.hpp"
+#include "utils/extension.hpp"
+#include "utils/keybinds.hpp"
+#include "utils/logging.hpp"
+#include "utils/timer.hpp"
 
 #ifdef DEBUG
   #include "utils/debug.hpp"
 #endif
 
-#include "constants.hpp"
-#include "settings.hpp"
-
 #include "camera.hpp"
+#include "constants.hpp"
 #include "environment.hpp"
 #include "interface.hpp"
-
+#include "settings.hpp"
 #include "window.hpp"
-
-#include "graphics/shaders.hpp"
-#include "graphics/renderInterface.hpp"
-
-#include "models/modelInterface.hpp"
-#include "models/modelStorage.hpp"
-
-#include "lighting/lightInterface.hpp"
-#include "lighting/lightStorage.hpp"
 
 #endif
