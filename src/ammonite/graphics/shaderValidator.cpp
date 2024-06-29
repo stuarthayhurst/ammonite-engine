ea0421cdd56a27490dd499d53e434a8e77ac09b9
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2024-06-29 16:58:32 +0100

    Reimplement strtok_r and strtok, use goto for clean up

--- src/ammonite/graphics/shaderValidator.cpp
+++ src/ammonite/graphics/shaderValidator.cpp
@@ -1,5 +1,4 @@
 #include <cstddef>
-#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <string>
@@ -10,6 +9,41 @@
 namespace ammonite {
   namespace shaders {
     namespace internal {
+      namespace {
+        /*
+         - Similar to strtok_r, except it only supports single character delimiters
+         - Unsafe if called with both input and (save or *save) as nullptr / undefined
+         - save must never be nullptr
+        */
+        static char* parseToken(char* input, char delim, char** save) {
+          //Restore from next byte to search
+          if (input == nullptr) {
+            input = *save;
+          }
+
+          //Programmer error or no more tokens
+          if (*input == '\0') {
+            return nullptr;
+          }
+
+          char* end = input;
+          //Search for delimiter or end of string
+          while (*end != delim && *end != '\0') {
+            end++;
+          }
+
+          //Last token found
+          if (*end == '\0') {
+            return input;
+          }
+
+          //Create a string, save state and return it
+          *end = '\0';
+          *save = end + 1;
+          return input;
+        }
+      }
+
       bool validateCache(unsigned char* data, std::size_t size, void* userPtr) {
         CacheInfo* cacheInfoPtr = (CacheInfo*)userPtr;
         unsigned char* dataCopy = new unsigned char[size];
@@ -20,90 +54,78 @@ namespace ammonite {
          - Decide whether the cache file can be used
          - Uses input files, sizes and timestamps
         */
-        char* state;
+        char* state = nullptr;
         int fileCount = 1;
-        char* token = strtok_r((char*)dataCopy, "\n", &state);
+        long long int filesize = 0, modificationTime = 0;
+        char* token = parseToken((char*)dataCopy, '\n', &state);
         do {
           //Give up if token is null, we didn't find enough files
           if (token == nullptr) {
-            delete [] dataCopy;
-            return false;
+            goto failed;
           }
 
           //Check first token is 'input'
           std::string currentFilePath = cacheInfoPtr->filePaths[fileCount - 1];
-          char* nestedToken = strtok(token, ";");
+          char* nestedState = nullptr;
+          char* nestedToken = parseToken(token, ';', &nestedState);
           if ((nestedToken == nullptr) ||
               (std::string(nestedToken) != std::string("input"))) {
-            delete [] dataCopy;
-            return false;
+            goto failed;
           }
 
           //Check token matches shader path
-          nestedToken = strtok(nullptr, ";");
-          if ((nestedToken == nullptr) ||
-              (std::string(nestedToken) != currentFilePath)) {
-            delete [] dataCopy;
-            return false;
+          nestedToken = parseToken(nullptr, ';', &nestedState);
+          if ((nestedToken == nullptr) || (std::string(nestedToken) != currentFilePath)) {
+            goto failed;
           }
 
           //Get filesize and time of last modification of the shader source
-          long long int filesize = 0, modificationTime = 0;
           if (!ammonite::files::internal::getFileMetadata(currentFilePath, &filesize,
                                                           &modificationTime)) {
-            //Failed to get the metadata
-            delete [] dataCopy;
-            return false;
+            goto failed;
           }
 
           //Check token matches file size
-          nestedToken = strtok(nullptr, ";");
-          if ((nestedToken == nullptr) ||
-              (std::atoll(nestedToken) != filesize)) {
-            delete [] dataCopy;
-            return false;
+          nestedToken = parseToken(nullptr, ';', &nestedState);
+          if ((nestedToken == nullptr) || (std::atoll(nestedToken) != filesize)) {
+            goto failed;
           }
 
           //Check token matches timestamp
-          nestedToken = strtok(nullptr, ";");
-          if ((nestedToken == nullptr) ||
-              (std::atoll(nestedToken) != modificationTime)) {
-            delete [] dataCopy;
-            return false;
+          nestedToken = parseToken(nullptr, ';', &nestedState);
+          if ((nestedToken == nullptr) || (std::atoll(nestedToken) != modificationTime)) {
+            goto failed;
           }
 
           //Get the next line
           if (cacheInfoPtr->fileCount > fileCount) {
-            token = strtok_r(nullptr, "\n", &state);
+            token = parseToken(nullptr, '\n', &state);
           }
           fileCount += 1;
         } while (cacheInfoPtr->fileCount >= fileCount);
 
         //Find binary format
-        token = strtok_r(nullptr, "\n", &state);
-        if (token != nullptr) {
-          cacheInfoPtr->binaryFormat = std::atoll(token);
-        } else {
-          delete [] dataCopy;
-          return false;
+        token = parseToken(nullptr, '\n', &state);
+        if (token == nullptr) {
+            goto failed;
         }
+        cacheInfoPtr->binaryFormat = std::atoll(token);
 
         //Find binary length
-        token = strtok_r(nullptr, "\n", &state);
-        if (token != nullptr) {
-          cacheInfoPtr->binaryLength = std::atoll(token);
-        } else {
+        token = parseToken(nullptr, '\n', &state);
+        if (token == nullptr) {
+            goto failed;
+        }
+        cacheInfoPtr->binaryLength = std::atoll(token);
+
+        if (cacheInfoPtr->binaryFormat != 0 && cacheInfoPtr->binaryLength != 0) {
           delete [] dataCopy;
-          return false;
+          return true;
         }
 
-        if (cacheInfoPtr->binaryFormat == 0 || cacheInfoPtr->binaryLength == 0) {
-          delete [] dataCopy;
-          return false;
-        }
-
+failed:
         delete [] dataCopy;
-        return true;
+        return false;
       }
     }
   }
