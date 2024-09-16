509503cc552a45a053df5297782836654160943e
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2024-09-16 15:24:11 +0100

    Document functions in thread.cpp

--- src/ammonite/utils/thread.cpp
+++ src/ammonite/utils/thread.cpp
@@ -4,19 +4,40 @@
 namespace ammonite {
   namespace utils {
     namespace thread {
+      /*
+       - Return the number of threads in the pool
+          - Returns 0 if the pool doesn't exist
+      */
       unsigned int getThreadPoolSize() {
         return ammonite::thread::internal::getThreadPoolSize();
       }
 
+      /*
+       - Submit a job to the thread pool, with a user-provided pointer
+         - userPtr may be a nullptr
+      */
       void submitWork(AmmoniteWork work, void* userPtr) {
         ammonite::thread::internal::submitWork(work, userPtr, nullptr);
       }
 
+      /*
+       - Submit a job to the thread pool, with a user-provided pointer and completion
+         - If completion isn't a nullptr, it should be initialised with ATOMIC_FLAG_INIT
+         - userPtr may be a nullptr
+      */
       void submitWork(AmmoniteWork work, void* userPtr, AmmoniteCompletion* completion) {
         ammonite::thread::internal::submitWork(work, userPtr, completion);
       }
 
-      //userBuffer and completions may be null
+      /*
+       - Submit multiple jobs to the thread pool, with a user-provided buffer and completions
+         - completions should either be a nullptr or an array of completions
+           - These should be initialised with ATOMIC_FLAG_INIT
+         - userBuffer should either be a nullptr, or an array of data to be split between jobs
+           - Each job will receive a section according to (userBuffer + job index * stride)
+           - stride should by the size of each section to give to a job, in bytes
+         - jobCount specifies how many times submit the job
+      */
       void submitMultiple(AmmoniteWork work, void* userBuffer, int stride,
                           AmmoniteCompletion* completions, unsigned int jobCount) {
         ammonite::thread::internal::submitMultiple(work, userBuffer, stride, completions, jobCount);
@@ -50,22 +71,27 @@ namespace ammonite {
         completion->clear();
       }
 
+      //Block the thread pool from starting newer jobs, return immediately
       void blockThreadsAsync() {
         ammonite::thread::internal::blockThreads(false);
       }
 
+      //Block the thread pool from starting newer jobs, return after it takes effect
       void blockThreadsSync() {
         ammonite::thread::internal::blockThreads(true);
       }
 
+      //Unblock the thread pool from starting newer jobs, return immediately
       void unblockThreadsAsync() {
         ammonite::thread::internal::unblockThreads(false);
       }
 
+      //Unblock the thread pool from starting newer jobs, return after it takes effect
       void unblockThreadsSync() {
         ammonite::thread::internal::unblockThreads(true);
       }
 
+      //Wait until all work in the pool as of the call is finished
       void finishWork() {
         ammonite::thread::internal::finishWork();
       }
