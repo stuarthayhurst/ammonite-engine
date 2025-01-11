035a2f1d7b8da1573dfa3aef11f69aa632682f0d
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2025-01-11 17:04:41 +0000

    Don't require OutputHelpers to be const

--- src/ammonite/utils/logging.cpp
+++ src/ammonite/utils/logging.cpp
@@ -2,8 +2,10 @@
 
 namespace ammonite {
   namespace utils {
+    //NOLINTBEGIN(cppcoreguidelines-avoid-non-const-global-variables)
     OutputHelper error(std::cerr, "ERROR: ");
     OutputHelper warning(std::cerr, "WARNING: ");
     OutputHelper status(std::cout, "STATUS: ");
+    //NOLINTEND(cppcoreguidelines-avoid-non-const-global-variables)
   }
 }
