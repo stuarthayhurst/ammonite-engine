6b52ce33a38f90e83f55a846254e12b497d79245
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2025-02-03 19:06:53 +0000

    Only hide symbols when supported by the compiler

--- src/ammonite/internal.hpp
+++ src/ammonite/internal.hpp
@@ -1,4 +1,10 @@
 #ifndef INTERNAL
 #define INTERNAL
-#define AMMONITE_INTERNAL __attribute__ ((visibility ("hidden")))
+
+#if __has_cpp_attribute(visibility)
+  #define AMMONITE_INTERNAL __attribute__ ((visibility ("hidden")))
+#else
+  #define AMMONITE_INTERNAL
+#endif
+
 #endif
