cedb1482589c94640a2d40582d451a1483601e24
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2025-07-01 01:50:53 +0100

    Test negative floats, fix negative comparisons

--- src/tests/support.hpp
+++ src/tests/support.hpp
@@ -16,7 +16,7 @@ void randomFillVector(ammonite::Vec<T, size>& vec) {
     } else if constexpr (std::is_integral_v<T>) {
       vec[i] = (T)ammonite::utils::randomInt((T)std::sqrt(std::numeric_limits<T>::max() / size));
     } else {
-      vec[i] = (T)ammonite::utils::randomDouble(1000000.0f);
+      vec[i] = (T)ammonite::utils::randomDouble(-1000000.0f, 1000000.0f);
     }
   }
 }
@@ -26,7 +26,7 @@ template <typename T, unsigned int cols, unsigned int rows>
 void randomFillMatrix(ammonite::Mat<T, cols, rows>& mat) {
   for (unsigned int col = 0; col < cols; col++) {
     for (unsigned int row = 0; row < rows; row++) {
-      mat[col][row] = (T)ammonite::utils::randomDouble(1000000.0f);
+      mat[col][row] = (T)ammonite::utils::randomDouble(-1000000.0f, 1000000.0f);
     }
   }
 }
@@ -36,7 +36,7 @@ bool roughly(T a, T b) {
   if constexpr (std::is_integral_v<T>) {
     return (a == b);
   } else {
-    return std::abs(a - b) <= std::max((T)1e-5f, std::max(a, b) * (T)0.001);
+    return std::abs(a - b) <= std::max((T)1e-5f, std::abs(std::max(a, b) * (T)0.001));
   }
 }
 
