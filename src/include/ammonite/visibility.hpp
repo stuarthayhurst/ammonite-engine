c53b92c9b1214509808a330599a9d4882726c8d2
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2025-12-05 20:47:05 +0000

    Raise an error when symbol visibility macros fail

--- src/include/ammonite/visibility.hpp
+++ src/include/ammonite/visibility.hpp
@@ -3,14 +3,11 @@
 
 #if __has_cpp_attribute(visibility)
   #define AMMONITE_EXPOSED __attribute__ ((visibility ("default")))
-#else
-  #define AMMONITE_EXPOSED
-#endif
-
-#if __has_cpp_attribute(visibility)
   #define AMMONITE_INTERNAL __attribute__ ((visibility ("hidden")))
 #else
+  #define AMMONITE_EXPOSED
   #define AMMONITE_INTERNAL
+  #error "Can't control symbol visibility, expect problems"
 #endif
 
 #endif
