ae33fb40077e051d3717ff724cd2cf181530d5a0
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2025-12-06 03:08:06 +0000

    Remove extra spacing in visibility macro

--- src/include/ammonite/visibility.hpp
+++ src/include/ammonite/visibility.hpp
@@ -2,8 +2,8 @@
 #define AMMONITEVISIBILITY
 
 #if __has_cpp_attribute(visibility) || __has_cpp_attribute(gnu::visibility)
-  #define AMMONITE_EXPOSED __attribute__ ((visibility ("default")))
-  #define AMMONITE_INTERNAL __attribute__ ((visibility ("hidden")))
+  #define AMMONITE_EXPOSED __attribute__((visibility("default")))
+  #define AMMONITE_INTERNAL __attribute__((visibility("hidden")))
 #else
   #define AMMONITE_EXPOSED
   #define AMMONITE_INTERNAL
