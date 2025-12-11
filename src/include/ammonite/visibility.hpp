df76d2326142ea466745e2bcd6d172d209a914ae
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2025-12-11 23:30:15 +0000

    Use __has_attribute for testing general attributes

--- src/include/ammonite/visibility.hpp
+++ src/include/ammonite/visibility.hpp
@@ -1,9 +1,12 @@
 #ifndef AMMONITEVISIBILITY
 #define AMMONITEVISIBILITY
 
-#if __has_cpp_attribute(visibility) || __has_cpp_attribute(gnu::visibility)
+#if __has_attribute(visibility)
   #define AMMONITE_EXPOSED __attribute__((visibility("default")))
   #define AMMONITE_INTERNAL __attribute__((visibility("hidden")))
+#elif __has_cpp_attribute(gnu::visibility)
+  #define AMMONITE_EXPOSED [[gnu::visibility("default")]]
+  #define AMMONITE_INTERNAL [[gnu::visibility("hidden")]]
 #else
   #define AMMONITE_EXPOSED
   #define AMMONITE_INTERNAL
