da4e585a464d9df5dc776295ed39fe44a12184fa
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-06-02 19:57:10 +0100

    Rework launch.sh
     - Drop singular spellings of arguments
     - Allow --loop and --valgrind to appear in any position
     - Filter launcher script arguments from passed arguments
     - Check the binary exists in loop mode
     - Minor output changes

--- launch.sh
+++ launch.sh
@@ -3,44 +3,72 @@
 buildDir="build"
 NEW_LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$buildDir"
 
-if [[ "$1" == "--loop" || "$1" == "--loops" ]]; then
-  if [[ "$2" == "math" || "$2" == "maths" ]]; then
-    target="$buildDir/mathsTest"
-  else
-    target="$buildDir/threadTest"
-  fi
-  while true; do
-    if ! LD_LIBRARY_PATH="$NEW_LD_LIBRARY_PATH" "$target" "$@"; then
-      exit 1
-    fi
-  done
-elif [[ "$1" == "--thread" || "$1" == "--threads" ]]; then
-  target="$buildDir/threadTest"
-elif [[ "$1" == "--math" || "$1" == "--maths" ]]; then
-  target="$buildDir/mathsTest"
-else
-  target="$buildDir/demo"
-fi
+target=""
+targetArgs=()
 
-#Pick a path to valgrind
-USE_VALGRIND_PATH=""
+loopRequested="false"
+valgrindRequested="false"
+
+#Process arguments, filtering out arguments handled by launch.sh
 for arg in "$@"; do
- if [[ "$arg" == "--valgrind" ]]; then
-   if [[ "$VALGRIND" != "" ]]; then
-     USE_VALGRIND_PATH="$VALGRIND"
-   else
-     USE_VALGRIND_PATH="valgrind"
-   fi
- fi
+  #Pick a target binary
+  if [[ "$arg" == "--threads" ]]; then
+    target="$buildDir/threadTest"
+    continue
+  elif [[ "$arg" == "--maths" ]]; then
+    target="$buildDir/mathsTest"
+    continue
+  fi
+
+  #Enable looping the target
+  if [[ "$arg" == "--loop" ]]; then
+    loopRequested="true"
+    continue
+  fi
+
+  #Run the target with valgrind
+  if [[ "$arg" == "--valgrind" ]]; then
+    valgrindRequested="true"
+    continue
+  fi
+
+  targetArgs+=("$arg")
 done
 
+#Default to running the demo, disable loop mode if triggered
+if [[ "$target" == "" ]]; then
+  target="$buildDir/demo"
+  loopRequested="false"
+fi
+
+#Check that the selected target exists
 if [[ ! -f "$target" ]]; then
-  echo "$target doesn't exist, did you forget to build the demo?" > /dev/stderr
+  echo "'$target' doesn't exist, did you forget to build it?" > /dev/stderr
   exit 1
 fi
 
-if [[ "$USE_VALGRIND_PATH" == "" ]]; then
-  LD_LIBRARY_PATH="$NEW_LD_LIBRARY_PATH" "$target" "$@"
+#Pick a path to valgrind, if requested
+USE_VALGRIND_PATH=""
+if [[ "$valgrindRequested" == "true" ]]; then
+  if [[ "$VALGRIND" != "" ]]; then
+    USE_VALGRIND_PATH="$VALGRIND"
+  else
+    USE_VALGRIND_PATH="valgrind"
+  fi
+fi
+
+#Run the target in a loop or just once
+if [[ "$loopRequested" == "true" ]]; then
+  while true; do
+    if ! LD_LIBRARY_PATH="$NEW_LD_LIBRARY_PATH" "$target" "${targetArgs[@]}"; then
+      exit 1
+    fi
+  done
 else
-  LD_LIBRARY_PATH="$NEW_LD_LIBRARY_PATH" "$USE_VALGRIND_PATH" --leak-check=full "$target" "$@"
+  #Use valgrind if configured to
+  if [[ "$USE_VALGRIND_PATH" == "" ]]; then
+    LD_LIBRARY_PATH="$NEW_LD_LIBRARY_PATH" "$target" "${targetArgs[@]}"
+  else
+    LD_LIBRARY_PATH="$NEW_LD_LIBRARY_PATH" "$USE_VALGRIND_PATH" --leak-check=full "$target" "${targetArgs[@]}"
+  fi
 fi
